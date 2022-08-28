294d5c1df4a1640f7345eb596e99771ad53d2e5c
Rodevp <rodriguezrodrigor12@gmail.com>
2022-08-27 22:07:49 -0500

    operadores

---
+++ operadores.go
@@ -0,0 +1,29 @@
+package main
+
+//import "math"
+
+func operators() {
+
+	/*
+		> mayor que
+		>= mayor o igual
+		< menor que
+		<= menor o igual
+		== igualdad
+
+		! negacion
+		|| OR
+		&& AND
+
+		aritmeticos
+		* multiplicacion
+		- resta
+		/ division
+		+ suma
+
+		//para más operadores debemos importar el modulo math
+		math.Abs(3.14)
+
+	*/
+
+}
